358ee03cca9dd30ab9fb4ec78e1e515a79ba0341
Jason Crockett <deddokatana@gmail.com>
2017-03-18 18:33:09 +0000

    zipping is working

--- RouteFinder.py
+++ RouteFinder.py
@@ -3,7 +3,7 @@ from itertools import chain,combinations,permutations,islice
 
 rates = dict(json.loads(open("sample.json","r").read()))
 
-
+# from itertools doc recipies - https://docs.python.org/3.6/library/itertools.html
 def powerset(iterable):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
@@ -12,6 +12,7 @@ def powerset(iterable):
 
 def generate_routes(rates):
     uniquecombos = list(powerset(rates.keys()))
+    uniquecombos.pop(0)
     shuffleduniquecombos = []
     print(uniquecombos)
     for t in uniquecombos:
@@ -23,13 +24,26 @@ def generate_routes(rates):
 
 
 def try_route(symbols:tuple,initialstake:float):
+    print("recieved symbols",symbols)
     subtotal = initialstake
     slice1 = islice(symbols,0,None,2)
     slice2 = islice(symbols,1,None,2)
+    zipped = list(zip(slice1,slice2))
+    print("zipped : ",zipped)
     for symbol1,symbol2 in list(zip(slice1,slice2)):
         subtotal = rates[symbol1][symbol2]
     return subtotal
 
-def try_routes(startcurrency,endcurrency,initialfloat):
+def try_routes(startcurrency:tuple,endcurrency,initialstake:float):
+    results = {}
     for route in generate_routes(rates):
-        try_route(route)
+        empty = ()
+        if route == empty:
+            pass
+        print(route)
+        if route[-1] == endcurrency and route[0] != startcurrency:
+            result = try_route(startcurrency + tuple(route),initialstake)
+            results[route] = result
+    return results
+
+print(try_routes(('GBP',),'GBP',1.0))
