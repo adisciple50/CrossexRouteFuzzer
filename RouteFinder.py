501648c7da73ebedd12842df959854989f2c0896
Jason Crockett <deddokatana@gmail.com>
2017-03-28 02:39:48 +0100

    Update RouteFinder.py

--- RouteFinder.py
+++ RouteFinder.py
@@ -7,7 +7,7 @@ rates = dict(json.loads(open("sample.json","r").read()))
 
 # from itertools doc recipies - https://docs.python.org/3.6/library/itertools.html
 def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    # "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
