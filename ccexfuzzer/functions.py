babc0626c14cb906aae723628c7e3a04fcf5be18
Jason Crockett <deddokatana@gmail.com>
2017-04-01 16:45:46 +0100

    added c-cex crossex as a package - added single threaded mode

---
+++ ccexfuzzer/functions.py
@@ -0,0 +1,73 @@
+# import json
+import requests
+# import RouteFinder
+from json.decoder import JSONDecodeError
+from http.client import RemoteDisconnected
+# from io import UnsupportedOperation,BufferedRandom
+import datetime
+
+# begin print available markets
+# markets = requests.get("https://c-cex.com/t/api_pub.html?a=getmarkets").json()
+# end markets
+
+
+# get coin names
+
+def get_coinnames(api_url:str='https://c-cex.com/t/coinnames.json'):
+    return set(dict(requests.get(api_url).json()).keys())
+
+# list file IO
+
+def read_list_file(filepath):
+    with open(filepath,'r') as to_read:
+        f = to_read.read().splitlines()
+    return list(f)
+
+def write_list_to_file(list:list,filepath):
+    with open(filepath, 'a+') as to_write:
+        to_write.writelines(list)
+    return True
+
+# generate urls to parse
+
+def generate_urls(coinsymbolset:set):
+    urls = []
+    for base in coinsymbolset:
+        for pair in coinsymbolset:
+            urls.append(str("https://c-cex.com/t/" + str(base) + "-" + str(pair) + ".json"))
+    return urls
+
+def apply_blacklist(list_to_filter:list,blacklist_filename:str='blacklist.list'):
+    blacklist = read_list_file(blacklist_filename)
+    return list(filter(lambda x: x in blacklist,list_to_filter))
+
+# fetch data,parse urls, sanitize data.
+
+def get_coin_buy_prices(api_urls:list):
+    i = 0
+    end = str(int(len(api_urls)))
+    exchangerates = {}
+    unfinished = [] # urls that werent attempted due to a remote server disconnect
+    blacklist = []
+    for url in api_urls:
+        base = url[str(url).rfind('/'):str(url).rfind('-')]  # rfinds start from 0 quirk to the rescue!
+        try:
+            i += 1
+            print(datetime.datetime.now(), 'Progress ', i, '/' , end , 'url is', url)
+            exchangerate = requests.get(url).json()
+
+            exchangerate = exchangerate['ticker']['buy']
+        except JSONDecodeError:
+            exchangerate = 0.0
+            blacklist.append(url)
+        except RemoteDisconnected:
+            exchangerate = 0.0
+            unfinished.append(url)
+        finally:
+            exchangerates[str(base)] = [{str(url): float(exchangerate)}]
+    return {'exchangerates':exchangerates,'unfinished':unfinished,'blacklist':blacklist}
+
+def apply_coin_equivelent_exchanges(coinsymbolset:set,exchangerates:dict): # ETH-ETH = 1 etc.
+    for base in coinsymbolset:
+        exchangerates[str(base)] = list({str(base):1.0}) + exchangerates[str(base)]
+    return exchangerates
---
+++ ccexfuzzer/fuzzer.py
@@ -0,0 +1,46 @@
+from ccexfuzzer.functions import *
+import json
+
+
+def run():
+
+    urls = []
+
+    try:
+        with open('urls.list', 'r'): # only checks if file exists
+            urls = read_list_file('urls.list')
+    except:
+        with open('urls.list', 'w+') as urlslist:
+            urls = generate_urls(get_coinnames())
+            urlslist.writelines(urls)
+
+
+    try:
+        with open('blacklist.list','r'):
+            urls = apply_blacklist(urls)
+    except Exception as e:
+        with open('blacklist.list', 'x'):
+            pass
+
+    results = get_coin_buy_prices(urls)
+
+    with open('ccex.json','w') as ccexjson:
+        ccexjson.writelines(json.dumps(results['exchangerates']))
+    write_list_to_file(results['blacklist'],'blacklist.list')
+    write_list_to_file(results['unfinished'],'unfinished.list') # writes urls that werent able to return data, because the server disconnected before the run.
+
+
+def resume_unfinished(unfinished:str='unfinished.list'):
+    results = get_coin_buy_prices(read_list_file(unfinished))
+
+    with open('ccex.json', 'w') as ccexjson:
+        ccexjson.writelines(json.dumps(results['exchangerates']))
+    with open(unfinished,'w') as ufile:
+        ufile.truncate() # clear infinished before rewriting.
+    write_list_to_file(results['blacklist'], 'blacklist.list')
+    write_list_to_file(results['unfinished'],'unfinished.list')  # writes urls that werent able to return data, because the server disconnected before the run.
+
+
+if __name__ == '__main__':
+    run()
+    resume_unfinished()
